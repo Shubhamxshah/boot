00956e12dcef2f1d7888defe354fc6f2edd4da08
shubhamxshah <ritikgujarat@gmail.com>
2026-03-21 20:38:02 +0530

    pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  apps: [
+    {
+      name: "backend",
+      cwd: "./backend",
+      script: "make",
+      args: "prod",
+      interpreter: "none",
+      env_file: "./backend/.env.prod",
+      autorestart: true,
+      watch: false,
+      log_date_format: "YYYY-MM-DD HH:mm:ss",
+    },
+    {
+      name: "frontend",
+      cwd: "./frontend",
+      script: "npm",
+      args: "run start",
+      interpreter: "none",
+      env: {
+        NODE_ENV: "production",
+        PORT: 3002,
+      },
+      autorestart: true,
+      watch: false,
+      log_date_format: "YYYY-MM-DD HH:mm:ss",
+    },
+  ],
+};
